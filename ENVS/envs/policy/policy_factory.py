ecf714078096d01588fb95999fb8e4f8807aff74
Chengmin Zhou <1083452798@qq.com>
2022-08-05 05:57:12 +0800

    Add files via upload

---
+++ ENVS/envs/policy/policy_factory.py
@@ -0,0 +1,14 @@
+from ENVS.envs.policy.orca import ORCA
+from ENVS.envs.policy.lstm_rl import LstmRL
+from ENVS.envs.policy.aw_per_a2c import AW_PER_A2C
+
+
+def none_policy():
+    return None
+
+
+policy_factory = dict()
+policy_factory['orca'] = ORCA
+policy_factory['lstm_rl'] = LstmRL
+policy_factory['aw_per_a2c'] = AW_PER_A2C
+policy_factory['none'] = none_policy
